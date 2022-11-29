62024da5453f93bfe3e5cf79b20adc610b1b61c5
Петър Стоев <petarstoev0505@gmail.com>
2022-11-29 15:29:03 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,27 +3,21 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-  });
-  
+  button.addEventListener("click", () => {});
 });
-
-
 
 let add_article = () => {
-  const articleTemplate = document.createElement('article');
-  articleTemplate.classList. add("message")
-  articleTemplate.innerHTML = " sample text  sample text  sample text  sample text";
+  const articleTemplate = document.createElement("article");
+  articleTemplate.classList.add("message");
+  articleTemplate.innerHTML =
+    " sample text  sample text  sample text  sample text";
   document.body.appendChild(articleTemplate);
+};
 
-}
-
-document.body.addEventListener('click', () => {
-  let flag=5
-    while(flag!=0) {
-      add_article()
-      flag--
-    }
+document.body.addEventListener("click", (event) => {
+  let flag = 5;
+  while (flag != 0) {
+    add_article();
+    flag--;
+  }
 });
-
-
